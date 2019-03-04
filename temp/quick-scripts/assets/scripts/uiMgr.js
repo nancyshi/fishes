9436c9c079d993fd567d5a6ccfb85f729d4c2556
edz <edz@houwan.local>
2019-03-04 09:59:31 +0800

    course commit , for area select function

---
+++ temp/quick-scripts/assets/scripts/uiMgr.js
@@ -0,0 +1,74 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/scripts/uiMgr.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, '05e0bAoHWhPEo3bbMD7NP8e', 'uiMgr', __filename);
+// scripts/uiMgr.js
+
+"use strict";
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        areaSelectPrefab: cc.Prefab,
+        areaSelectButton: cc.Node
+
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function onLoad() {},
+    start: function start() {},
+
+
+    // update (dt) {},
+
+    onClickAreaSelectButton: function onClickAreaSelectButton() {
+        var areaSelectUI = cc.instantiate(this.areaSelectPrefab);
+        var background = areaSelectUI.getChildByName("background");
+        var backArea = areaSelectUI.getChildByName("backArea");
+        var xOffset = (background.getContentSize().width + backArea.getContentSize().width) / 2;
+        areaSelectUI.setPosition(-xOffset, 0);
+        this.node.addChild(areaSelectUI);
+
+        var moveAction = cc.moveTo(0.2, cc.v2(0, 0));
+        areaSelectUI.runAction(moveAction);
+    }
+});
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=uiMgr.js.map
+        
